92aba18932a8b1ac33aba49279d3ef2aa55623f8
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-02-25 22:43:05 +0530

    added rutes for MainMenuComponent, OptionsComponent, GameScreenComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { OptionsComponent } from './components/options/options.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { GameScreenComponent } from './components/game-screen/game-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "main-menu", pathMatch: "full"},
+    {path: 'main-menu', component: MainMenuComponent, title: "Main Menu"},
+    {path: 'options', component: OptionsComponent, title: "Options"},
+    {path: 'game-screen', component: GameScreenComponent, title: "Game"},
+    {path: '**', redirectTo: "main-menu", pathMatch: "full"}
+];
