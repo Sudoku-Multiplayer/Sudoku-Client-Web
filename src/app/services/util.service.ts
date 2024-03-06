e8f912d1bbb0b39a1a9331104c7c0102d98fc96c
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-03-06 17:21:39 +0530

    Util service class.

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+    public clone2DArray(array: any[][]): any[][] {
+        let newArray = new Array(array.length);
+        for (let i = 0; i < array.length; i++) {
+            newArray[i] = new Array(array[i].length);
+            for (let j = 0; j < array[i].length; j++) {
+                newArray[i][j] = array[i][j];
+            }
+        }
+
+        return newArray;
+    }
+
+}
