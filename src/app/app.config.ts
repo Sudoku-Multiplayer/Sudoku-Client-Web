7f6095e3771fb25ab0603a4d2f590a037bd92e3d
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-03-19 22:23:26 +0530

    Added providers for AnimationsAsync and HttpClient.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideRouter(routes),
+  provideAnimationsAsync(),
+  provideHttpClient()
+  ]
 };
