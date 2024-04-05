d580b0a6384bbb6094a4e45d0da10fea4a93a295
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-05 21:42:00 +0530

    Guard to prevent unauthorized access.

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const router: Router = inject(Router);
+  const authService: AuthService = inject(AuthService);
+
+  if (authService.isLoggedIn()) {
+    return true;
+  }
+
+  router.navigate(['login']);
+  return false;
+};
