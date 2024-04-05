f9200c336c1e8de5eab9ca2cc3326b8f20861d2c
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-05 21:40:39 +0530

    Guard for game screen component.

---
+++ src/app/guards/game-screen.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { GameStateService } from '../services/game-state.service';
+
+export const gameScreenGuard: CanActivateFn = (route, state) => {
+
+  let gameStateService: GameStateService = inject(GameStateService);
+  let router: Router = inject(Router);
+
+  if (gameStateService.canEnterGameScreenForMultiplayer()) {
+    return true;
+  }
+
+  if (gameStateService.canEnterGameScreenForSingleplayer()) {
+    return true;
+  }
+
+  router.navigate(['main-menu']);
+  return false;
+};
