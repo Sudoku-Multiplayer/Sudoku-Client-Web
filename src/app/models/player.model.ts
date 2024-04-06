b30f3fb3e9780a24096a1cd18a5780001a399736
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-06 23:29:33 +0530

    Added playerType member variable.

--- src/app/models/player.model.ts
+++ src/app/models/player.model.ts
@@ -1,9 +1,13 @@
+import { PlayerType } from "../enums/player-type";
+
 export class Player{
     name: string;
     id: number;
+    playerType: PlayerType;
 
-    constructor(name: string, id: number){
+    constructor(name: string, id: number, playerType: PlayerType){
         this.name = name;
         this.id = id;
+        this.playerType = playerType;
     }
 }
