ba32a9da8f81f889869c5f59c6f7d89b80938c20
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-06 23:28:11 +0530

    Model to store data for a authenticated player.

---
+++ src/app/models/player-data.model.ts
@@ -0,0 +1,13 @@
+export class PlayerData {
+
+    constructor(public id?: number,
+        public playerName?: string,
+        public email?: string,
+        public password?: string,
+        public createdOn?: Date,
+        public lastUpdatedOn?: Date
+    ) {
+
+    }
+    
+}
