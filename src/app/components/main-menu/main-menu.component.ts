7d59e303836c00417440d1c3fcdf316665540590
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-06 22:49:25 +0530

    Added multiPlayerGame() and settings() method.

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { Router } from '@angular/router';
+import { MultiplayerScreenComponent } from '../multiplayer-screen/multiplayer-screen.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDividerModule],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, MultiplayerScreenComponent],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
@@ -19,12 +21,12 @@ export class MainMenuComponent {
     this.router.navigate(['options']);
   }
 
-  multiPlayerGame(){
-
+  multiPlayerGame() {
+    this.router.navigate(['multiplayer-screen']);
   }
 
-  settings(){
-    
+  settings() {
+    this.router.navigate(['settings']);
   }
 
 }
