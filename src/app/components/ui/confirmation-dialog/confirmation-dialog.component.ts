04873f5f19f73fca83ee9a5ca84158dd0255e9d6
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-04-06 23:20:44 +0530

    Component for conformation dialog.

---
+++ src/app/components/ui/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,47 @@
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { ConfirmationDialogData } from '../../../models/confirmation-dialog-data.model';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.css'
+})
+export class ConfirmationDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public confirmationDialogData: ConfirmationDialogData
+  ) {
+
+  }
+
+  no() {
+    this.dialogRef.close(false);
+  }
+
+  yes() {
+    this.dialogRef.close(true);
+  }
+
+}
