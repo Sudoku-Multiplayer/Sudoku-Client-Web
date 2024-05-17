44a84c1f607c22ea28d30960e30fd1100e1dfb38
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-17 20:25:16 +0530

    feat: Add WebSocketService provider to application configuration
    - Imported WebSocketService and webSocketServiceFactory to app configuration.
    - Added WebSocketService provider using useFactory in ApplicationConfig.
    - Ensures WebSocketService is properly configured and available for dependency injection.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { WebSocketService } from './services/web-socket.service';
+import { webSocketServiceFactory } from './configs/websocket-service-factory';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
   provideAnimationsAsync(),
-  provideHttpClient()
+  provideHttpClient(),
+  {
+    provide: WebSocketService,
+    useFactory: webSocketServiceFactory,
+  }
   ]
 };
