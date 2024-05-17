10d6beb562085f966776c95fb73e567f7a829745
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-17 21:21:01 +0530

    feat: Add authGuard to main-menu route and rename gameScreenGuard
    - Added authGuard to the main-menu route to ensure authentication.
    - Renamed gameScreenGuard to game-screen.guard for consistency.
    - Ensures that the main menu is only accessible to authenticated users.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import { OptionsComponent } from './components/options/options.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { GameScreenComponent } from './components/game-screen/game-screen.component';
-import { gameScreenGuard } from './guards/gameScreenGuard';
 import { MultiplayerScreenComponent } from './components/multiplayer-screen/multiplayer-screen.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { authGuard } from './guards/auth.guard';
+import { gameScreenGuard } from './guards/game-screen.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: "main-menu", pathMatch: "full" },
-    { path: 'main-menu', component: MainMenuComponent, title: "Main Menu" },
+    { path: 'main-menu', component: MainMenuComponent, title: "Main Menu", canActivate: [authGuard] },
     { path: 'login', component: LoginComponent, title: "Login" },
     { path: 'signup', component: SignupComponent, title: "Signup" },
     { path: 'options', component: OptionsComponent, title: "Options" },
