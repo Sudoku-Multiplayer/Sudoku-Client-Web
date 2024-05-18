dfc2cae6d1d4a5e8650a4fdf2c599268c47f43bc
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-18 20:12:21 +0530

    feat: Expand ServerConfig with additional server URLs and paths
    - Add GAMECHAT_SERVER_URL, GAME_SERVER_URL, and PLAYER_SERVER_URL constants.
    - Add GAMESESSION_BROKER_PATH and GAME_APPLICATION_DESTINATION_PREFIX constants.

--- src/app/configs/server.config.ts
+++ src/app/configs/server.config.ts
@@ -11,7 +11,17 @@ export class ServerConfig {
 
     readonly SUDOKU_SERVER_URL: String = this.BASE_SERVER_URL + "/sudoku-server";
 
+    readonly GAMECHAT_SERVER_URL: String = this.BASE_SERVER_URL + "/chat-server";
+
+    readonly GAME_SERVER_URL: String = this.BASE_SERVER_URL + "/game-server";
+
+    readonly PLAYER_SERVER_URL: String = this.BASE_SERVER_URL + "/player-server";
+
     readonly CHAT_PATH: string = "/game/chat";
 
     readonly BOARDUPDATE_PATH: string = "/game/board-updates";
+
+    readonly GAMESESSION_BROKER_PATH: string = "/game-session";
+
+    readonly GAME_APPLICATION_DESTINATION_PREFIX: string = "/game";
 }
