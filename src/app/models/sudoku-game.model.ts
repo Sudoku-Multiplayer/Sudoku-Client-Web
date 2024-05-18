416d1779be667344805818fb879b1955380e4883
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-18 20:16:19 +0530

    refactor: Update SudokuGame model to include hostPlayer, players, timeLimit, and remainingTime
    - Rename hostName property to hostPlayer and change its type to Player.
    - Add players property to store the list of players in the game.
    - Include timeLimit and remainingTime properties to manage time-related aspects of the game.
    - Adjust the constructor to accept the new properties and initialize them accordingly.

--- src/app/models/sudoku-game.model.ts
+++ src/app/models/sudoku-game.model.ts
@@ -1,21 +1,39 @@
 import { Level } from "../enums/level";
-import { GameStatus } from "../enums/game-status";
+import { Player } from "./player.model";
+import { GameSessionStatus } from "../enums/game-session-status";
 
 export class SudokuGame {
-    hostName: string;
+    hostPlayer: Player;
     boardSize: number;
     level: Level;
     playerCount: number;
     playerLimit: number;
-    status: GameStatus;
+    status: GameSessionStatus;
     gameId: string;
     gameName: string;
     initialBoard: number[][];
     currentBoard: number[][]
     solution: number[][];
+    players: Player[];
+    timeLimit: number;
+    remainingTime: number;
 
-    constructor(hostName: string, boardSize: number, level: Level, playerCount: number, playerLimit: number, status: GameStatus, gameId: string, gameName: string, initialBoard: number[][], currentBoard: number[][], solution: number[][]) {
-        this.hostName = hostName;
+    constructor(hostPlayer: Player,
+        boardSize: number,
+        level: Level,
+        playerCount: number,
+        playerLimit: number,
+        status: GameSessionStatus,
+        gameId: string,
+        gameName: string,
+        initialBoard: number[][],
+        currentBoard: number[][],
+        solution: number[][],
+        players: Player[],
+        timeLimit: number,
+        remainingTime: number
+    ) {
+        this.hostPlayer = hostPlayer;
         this.boardSize = boardSize;
         this.level = level;
         this.playerCount = playerCount;
@@ -26,5 +44,8 @@ export class SudokuGame {
         this.initialBoard = initialBoard;
         this.currentBoard = currentBoard;
         this.solution = solution;
+        this.players = players;
+        this.timeLimit = timeLimit;
+        this.remainingTime = remainingTime;
     }
 }
