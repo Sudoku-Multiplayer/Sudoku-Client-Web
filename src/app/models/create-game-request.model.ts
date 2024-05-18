553540bd27a83e3434c2635a3dd9c23030039a95
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-18 16:52:33 +0530

    feat: Add timeLimit member variable

--- src/app/models/create-game-request.model.ts
+++ src/app/models/create-game-request.model.ts
@@ -7,12 +7,14 @@ export class CreateGameRequest {
     boardSize: number;
     level: Level;
     playerLimit: number;
+    timeLimit: number;
 
-    constructor(player: Player, gameName: string, boardSize: number, level: Level, playerLimit: number) {
+    constructor(player: Player, gameName: string, boardSize: number, level: Level, playerLimit: number, timeLimit: number) {
         this.player = player;
         this.gameName = gameName;
         this.boardSize = boardSize;
         this.level = level;
         this.playerLimit = playerLimit;
+        this.timeLimit = timeLimit;
     }
 }
