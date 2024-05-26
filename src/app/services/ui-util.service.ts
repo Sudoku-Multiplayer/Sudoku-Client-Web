be42dfc3173f7dd9ade20dcc77d1f19483fefaf4
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-26 20:21:44 +0530

    feat: Add method to show 'About' dialog in UiUtilService
    - Added dependency injection for MatDialog.
    - Created showAboutDialog() method to open the AboutDialogComponent.

--- src/app/services/ui-util.service.ts
+++ src/app/services/ui-util.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { AboutDialogComponent } from "../components/ui/about-dialog/about-dialog.component";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UiUtilService {
+
+    dialog: MatDialog = inject(MatDialog);
 
     constructor(private _snackBar: MatSnackBar) {
 
@@ -14,6 +18,10 @@ export class UiUtilService {
         this._snackBar.open(message, ok, {
             duration: durationSec * 1000
         });
+    }
+
+    showAboutDialog() {
+        this.dialog.open(AboutDialogComponent);
     }
 
 }
