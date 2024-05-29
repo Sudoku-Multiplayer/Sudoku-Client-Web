1c35e51aba773cd57dad690943702182aeffc4c9
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2024-05-29 23:42:09 +0530

    feat: Add GameScreenSettings model to manage display preferences

---
+++ src/app/models/game-screen-settings.model.ts
@@ -0,0 +1,11 @@
+export class GameScreenSettings {
+
+    constructor(
+        public showBoardUpdates: boolean,
+        public autoScrollBoardUpdates: boolean,
+        public showGameChat: boolean,
+        public autoScrollGameChat: boolean) {
+
+    }
+
+}
